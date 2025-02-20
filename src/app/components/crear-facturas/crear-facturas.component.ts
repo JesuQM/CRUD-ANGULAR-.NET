ce96b12a83a24badd92ee9332dd0113d450637dd
JesuQM <jesusdavidqmq@gmail.com>
2025-02-20 17:19:23 -0500

    Correcciones Creacion Factura

--- src/app/components/crear-facturas/crear-facturas.component.ts
+++ src/app/components/crear-facturas/crear-facturas.component.ts
@@ -1,10 +1,10 @@
-import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { DetalleFactura } from 'src/app/entity/detalle-factura.model';
 import { Factura } from 'src/app/entity/factura.model';
 import { DetalleFacturaService } from 'src/app/services/detalle-factura.service';
 import { FacturasService } from 'src/app/services/facturas.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-crear-facturas',
@@ -22,13 +22,13 @@ export class CrearFacturasComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private facturaService: FacturasService, private detalleFacturaService: DetalleFacturaService) {
       this.facturaForm = this.fb.group({
-          cliente: ['', Validators.required],
+          cliente: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)]],
           fecha: ['', Validators.required],
           total: [{ value: '', disabled: true }, Validators.required],
           detalleFactura: this.fb.group({
             facturaId: [''],
             producto: [{ value: '', disabled: true }, Validators.required],
-            cantidad: ['', Validators.required],
+            cantidad: ['', [Validators.required, Validators.min(1), this.cantidadValidator]],   
             precioUnitario: [{ value: '', disabled: true }, Validators.required],
             subtotal: [{ value: '', disabled: true }, Validators.required]
           })
@@ -36,6 +36,11 @@ export class CrearFacturasComponent implements OnInit {
       
       this.facturaForm.get('detalleFactura.cantidad')?.valueChanges.subscribe(value => this.modificarTotal());
       this.facturaForm.get('detalleFactura.precioUnitario')?.valueChanges.subscribe(value => this.modificarTotal());
+  }
+
+  cantidadValidator(control: AbstractControl){
+    const value = control.value;
+    return value > 0 ? null : { cantidadInvalida: true };
   }
 
   ngOnInit(): void {
@@ -63,35 +68,45 @@ export class CrearFacturasComponent implements OnInit {
     });
   }
 
-    
-  
 
   async submit() {
+    Swal.showLoading();
     if (this.facturaForm.valid) {
         const facturaData: Factura = {
-          id: 0,
-          cliente: this.facturaForm.get('cliente')?.value,
-          fecha: this.facturaForm.get('fecha')?.value,
-          total: this.facturaForm.get('total')?.value
+            id: 0,
+            cliente: this.facturaForm.get('cliente')?.value,
+            fecha: this.facturaForm.get('fecha')?.value,
+            total: this.facturaForm.get('total')?.value
         };
         try {
             const facturaId = await this.facturaService.createFactura(facturaData).toPromise();
             const detalle: DetalleFactura = {
-              id:0,
-              facturaId: facturaId!,
-              producto: this.facturaForm.get('detalleFactura.producto')?.value,
-              cantidad: this.facturaForm.get('detalleFactura.cantidad')?.value,
-              precioUnitario: this.facturaForm.get('detalleFactura.precioUnitario')?.value,
-              subtotal: this.facturaForm.get('detalleFactura.subtotal')?.value
+                id: 0,
+                facturaId: facturaId!,
+                producto: this.facturaForm.get('detalleFactura.producto')?.value,
+                cantidad: this.facturaForm.get('detalleFactura.cantidad')?.value,
+                precioUnitario: this.facturaForm.get('detalleFactura.precioUnitario')?.value,
+                subtotal: this.facturaForm.get('detalleFactura.subtotal')?.value
             };
             await this.detalleFacturaService.addDetallesFactura(detalle).toPromise();
+            Swal.close();
+            Swal.fire({
+                title: '¡Éxito!',
+                text: 'La factura ha sido creada.',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            });
         } catch (error) {
             console.error('Error al crear la factura y los detalles:', error);
+            Swal.close();
+            Swal.fire({
+                title: 'Error',
+                text: 'Hubo un problema al crear la factura.',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
         }
         this.facturaForm.reset();
     }
-
-
-  }
-
+}
 }
